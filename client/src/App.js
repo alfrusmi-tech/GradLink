98de0b419efffd458fb58305de1251c340cafaa6
alfrusmi-tech <alfrusmi@gmail.com>
2026-07-04 20:19:26 +0530

    create a client (frontend)

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GradLink</h1>
+      <p>Smart Job Portal with CV Analysis</p>
+    </div>
+  );
+}
+
+export default App;
